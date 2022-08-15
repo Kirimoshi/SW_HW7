0d4f6b4cf560cf393c07b95a22e7680727c74ec6
Kirimoshi <baronaxaro@gmail.com>
2022-08-15 14:21:35 +0300

    Second SW3 commit, added explicit type coercion of input values to number

--- SW_HW3/homework/js/task1.js
+++ SW_HW3/homework/js/task1.js
@@ -1,16 +1,15 @@
-let initAmount = prompt('Please input initial amount of money');
-let numberOfYears = prompt('Please input number of years for your deposit. It should be integer (1,2,3) and ' +
-    'equal to or greater than 1');
-let percentagePerYear = prompt('Please input percentage of year. It should not be more than 100%');
+let initAmount = Number(prompt('Please input initial amount of money'));
+let numberOfYears = Number(prompt('Please input number of years for your deposit. It should be integer (1,2,3) and ' +
+    'equal to or greater than 1'));
+let percentagePerYear = Number(prompt('Please input percentage of year. It should not be more than 100%'));
 
-if (typeof initAmount !== 'number' || typeof numberOfYears !== 'number' || typeof percentagePerYear !== 'number' ||
-    initAmount < 1000 || numberOfYears < 1 || percentagePerYear > 100) {
+if (initAmount < 1000 || numberOfYears < 1 || percentagePerYear > 100) {
     alert('Invalid input data');
 } else {
     let totalAmount = initAmount * Math.pow(1 + percentagePerYear / 100, Number(numberOfYears));
     let totalProfit = totalAmount - initAmount;
-    let resultMessage = 'Initial amount: ' + initAmount + '\nNumber of years: ' + numberOfYears +
-        '\nPercentage of year: ' + percentagePerYear + '\n\nTotal profit: ' + totalProfit.toFixed(2) +
+    let resultMessage = 'Initial amount: ' + initAmount.toFixed(2) + '\nNumber of years: ' + numberOfYears +
+        '\nPercentage of year: ' + percentagePerYear.toFixed(2) + '\n\nTotal profit: ' + totalProfit.toFixed(2) +
         '\nTotal amount: ' + totalAmount.toFixed(2);
 
     alert(resultMessage);
