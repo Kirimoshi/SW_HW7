499497968515f43555de4cf66491729accb73e5f
Kirimoshi <baronaxaro@gmail.com>
2022-08-19 13:42:39 +0300

    First SW5 commit, 3/7 tasks done

---
+++ SW_HW5/homework/index.js
@@ -0,0 +1,20 @@
+function isEquals(arg1, arg2) {
+    return arg1 === arg2;
+}
+
+function isBigger(arg1, arg2) {
+    return arg1 > arg2;
+}
+
+function storeNames() {
+
+}
+
+function getDifference(arg1, arg2) {
+    return arg1 > arg2 ? arg1 - arg2 : arg2 - arg1;
+}
+
+isEquals();
+isBigger();
+storeNames();
+getDifference();
