3e08732133e5b439f90b92606325a93e9e81dd18
Kirimoshi <baronaxaro@gmail.com>
2022-08-19 12:08:22 +0300

    Third SW4 commit, 5/7 tasks done

--- SW_HW4/homework/app.js
+++ SW_HW4/homework/app.js
@@ -50,14 +50,57 @@ function filter(arr, func) {
     return filteredArray;
 }
 
-/*
-function getAdultAppleLovers(data) {
+data = [
+    {
+        "_id": "5b5e3168c6bf40f2c1235cd6",
+        "index": 0,
+        "age": 39,
+        "eyeColor": "green",
+        "name": "Stein",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e3168e328c0d72e4f27d8",
+        "index": 1,
+        "age": 38,
+        "eyeColor": "blue",
+        "name": "Cortez",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5b5e3168cc79132b631c666a",
+        "index": 2,
+        "age": 2,
+        "eyeColor": "blue",
+        "name": "Suzette",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e31682093adcc6cd0dde5",
+        "index": 3,
+        "age": 17,
+        "eyeColor": "green",
+        "name": "Weiss",
+        "favoriteFruit": "banana"
+    }
+]
 
+function getAdultAppleLovers(data) {
+    const filteredArray = filter(data, func);
+    function func (el) {
+        return (el.age >= 18) && (el.favoriteFruit === "apple");
+    }
+
+    return map(filteredArray, (el) => {
+        return el.name
+    });
 }
 
 function getKeys(obj) {
 
 }
+
+/*
 
 function getValues(obj) {
 
