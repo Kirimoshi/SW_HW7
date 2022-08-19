41a9a7a9f13fadc2a29ea8c488b03c094cbb4aa3
Kirimoshi <baronaxaro@gmail.com>
2022-08-19 13:03:57 +0300

    Fifth final SW4 commit, 7/7 tasks done, eslint checked

--- SW_HW4/homework/app.js
+++ SW_HW4/homework/app.js
@@ -26,14 +26,16 @@ function reverseNumber(num) {
 
 function forEach(arr, func) {
     for (let i = 0; i < arr.length; i++) {
-        func (arr[i]);
+        func(arr[i]);
     }
 }
 
 function map(arr, func) {
     const copyArray = [];
 
-    forEach(arr, (el) => { return copyArray.push(func(el)) })
+    forEach(arr, (el) => {
+ return copyArray.push(func(el)) 
+})
 
     return copyArray;
 }
@@ -50,45 +52,11 @@ function filter(arr, func) {
     return filteredArray;
 }
 
-data = [
-    {
-        "_id": "5b5e3168c6bf40f2c1235cd6",
-        "index": 0,
-        "age": 39,
-        "eyeColor": "green",
-        "name": "Stein",
-        "favoriteFruit": "apple"
-    },
-    {
-        "_id": "5b5e3168e328c0d72e4f27d8",
-        "index": 1,
-        "age": 38,
-        "eyeColor": "blue",
-        "name": "Cortez",
-        "favoriteFruit": "strawberry"
-    },
-    {
-        "_id": "5b5e3168cc79132b631c666a",
-        "index": 2,
-        "age": 2,
-        "eyeColor": "blue",
-        "name": "Suzette",
-        "favoriteFruit": "apple"
-    },
-    {
-        "_id": "5b5e31682093adcc6cd0dde5",
-        "index": 3,
-        "age": 17,
-        "eyeColor": "green",
-        "name": "Weiss",
-        "favoriteFruit": "banana"
-    }
-]
-
 function getAdultAppleLovers(data) {
     const filteredArray = filter(data, func);
     function func (el) {
-        return (el.age >= 18) && (el.favoriteFruit === "apple");
+        let ageLimit = 18;
+        return el.age >= ageLimit && el.favoriteFruit === 'apple';
     }
 
     return map(filteredArray, (el) => {
@@ -100,7 +68,9 @@ function getKeys(obj) {
     const keysArray = [];
 
     for (let key in obj) {
-        keysArray.push(key);
+        if (key) {
+            keysArray.push(key);
+        }
     }
 
     return keysArray;
@@ -110,8 +80,15 @@ function getValues(obj) {
     const valuesArray = [];
 
     for (let key in obj) {
-        valuesArray.push(obj[key]);
+        if (key) {
+            valuesArray.push(obj.key);
+        }
     }
 
     return valuesArray;
 }
+
+reverseNumber();
+getAdultAppleLovers();
+getKeys();
+getValues();
