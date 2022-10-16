7a7d05bc01dac74682340bce5cfeefb346c6c25a
Kirimoshi <baronaxaro@gmail.com>
2022-10-16 20:45:29 +0300

    Fourth SW6 commit, 2/2 tasks done, checked w/ ESLint

--- SW_HW6/homework/js/pipe.js
+++ SW_HW6/homework/js/pipe.js
@@ -4,14 +4,9 @@ function isFunction(functionToCheck) {
 
 const pipe = (value, ...funcs) => {
 	return funcs.reduce(function(acc, currentFunction, currentIndex) {
-		try {
 			if (!isFunction(currentFunction)) {
 				throw new Error(`Provided argument at position ${currentIndex} is not a function`);
 			}
-		} catch (e) {
-			alert(e);
-		}
-
 		return currentFunction(acc)
 	}, value)
 }
