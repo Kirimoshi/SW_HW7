d7d7d2da35f23da894502ed7fa6464d6ae80aa03
Kirimoshi <baronaxaro@gmail.com>
2022-12-08 11:08:12 +0200

    4th SW19 commit

--- SW_HW19/homework/js/index.js
+++ SW_HW19/homework/js/index.js
@@ -6,6 +6,21 @@ function addLogLine(result) {
     [logEquation, screenBlockContent] = [screenBlockContent, result];
     $( "#screen-block" ).val(result);
 
+    /*let newLogLine = document.createElement('div');
+    $( newLogLine ).addClass('logs-block__line');
+
+    $( newLogLine ).html(
+        "<div class=\"logs-block__empty-circle\">\n" +
+        "          <svg class=\"logs-block__empty-circle-svg\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z\"/></svg>\n" +
+        "        </div>" +
+        "" +
+        "<div class=\"logs-block__close-sign\">\n" +
+        "          <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 320 512\"><path d=\"M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z\"/></svg>\n" +
+        "        </div>"
+    );
+
+    $( '.logs-block' ).prepend( newLogLine );*/
+
     let newLogLine = document.createElement('div');
     $( newLogLine ).addClass('logs-block__line');
 
@@ -14,7 +29,12 @@ function addLogLine(result) {
     $( newLogLine ).append(emptyCircleSign);
 
     let newLogEquation = document.createElement('span');
+    $( newLogEquation ).css('margin-right', 'auto');
+    $( newLogEquation ).css('margin-left', 'auto');
     newLogEquation.textContent = (`${logEquation}=${result}`);
+    if ( newLogEquation.textContent.includes('48') ) {
+        $( newLogEquation ).css('text-decoration', 'underline');
+    }
     newLogLine.appendChild(newLogEquation);
 
     let closeSign = $( '.logs-block__close-sign' ).clone().removeClass('logs-block__close-sign')
@@ -85,12 +105,39 @@ $( ".numbers-block" ).click(function( event ) {
     }
 });
 
-$( '.logs-block__empty-circle-svg').click(function() {
-    $( this ).toggleClass('bg-red');
+$( '.logs-block' ).scroll(function() {
+    console.log(`Scroll Top: ${$('.logs-block').scrollTop()}`)
 });
 
-// TODO ScrollTop console
+/*$( '.logs-block__empty-circle-svg' ).hover(
+    function() {
+        $( this ).css('cursor', 'pointer');
+        $( this ).css('background-color', 'red');
+    },
+    function() {
+        $( this ).css('cursor', 'none');
+        $( this ).css('background-color', 'white');
+    }
+)*/
 
-/*setTimeout(() => {
-    console.log($('.logs-block').scrollTop());
-}, 100);*/
+// TODO Toggle background-color
+
+$( '.logs-block__empty-circle-svg' ).click(function( event ) {
+    $( event.target ).toggleClass('bg-red');
+});
+
+// TODO Removal of log line
+
+/*$( document ).ready(function() {
+    $( ".logs-block" ).click(function( event ) {
+        if ( $( event.target ).hasClass("logs-block__close-sign-clone") ) {
+            $( event.target ).parent().remove();
+        }
+    });
+});*/
+
+$( ".logs-block" ).on('click', function( event ) {
+    if ( $( event.target ).hasClass("logs-block__close-sign") ) {
+        $( event.target ).parent().remove();
+    }
+})
